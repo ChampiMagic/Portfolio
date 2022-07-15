142362098fe4cc6a57736e806a7fdf11784d456a
homero <homerogazze1015@gmail.com>
2022-07-15 15:14:46 -0300

    [FIRST] Boilerplate Created

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './normalize.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  
+  <React.StrictMode>
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/resume" element={<App />}/>
+        </Routes>
+      </Router>
+  </React.StrictMode>
+)
