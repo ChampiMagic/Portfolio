0293ec419ba056954919f04c704933a964aebaa4
homero <homerogazze1015@gmail.com>
2022-07-15 20:41:24 -0300

    [NEW] Adding last Ghost Structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import style from './App.module.css'
 import Presentation from './components/presentation/presentation'
 import Navigation from './components/navigation/navigation'
 import Article from './components/article/article'
+import AsideLeft from './components/asides/asideLeft/asideLeft'
+import AsideRight from './components/asides/asideRight/asideRight'
 
 
 
@@ -12,7 +14,9 @@ export default function App(): JSX.Element {
   return (
    <div className={style.body}>
     <Navigation />
+    <AsideLeft />
     <Article />
+    <AsideRight />
    </div>
    
   )
