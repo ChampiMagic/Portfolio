d6fe7855667ebbde992803159add7ceb28be4050
homero <homerogazze1015@gmail.com>
2022-07-16 20:50:49 -0300

    [NEW] Projects section Component Created

--- src/components/article/article.tsx
+++ src/components/article/article.tsx
@@ -3,6 +3,7 @@ import style from './article.module.css'
 import Presentation from './presentation/presentation'
 import About from './about/about'
 import Experience from './experience/experience'
+import Projects from './projects/projects'
 
 
 export default function Article(): JSX.Element {
@@ -12,6 +13,7 @@ export default function Article(): JSX.Element {
             <Presentation />
             <About />
             <Experience />
+            <Projects />
         </div>
        
     )
