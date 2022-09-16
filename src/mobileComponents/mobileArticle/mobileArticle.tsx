18553dac8f8408a796962867b2cddcebb78c4974
homero <homerogazze1015@gmail.com>
2022-09-16 14:43:45 -0300

    [NEW] MobileContact component created

--- src/mobileComponents/mobileArticle/mobileArticle.tsx
+++ src/mobileComponents/mobileArticle/mobileArticle.tsx
@@ -3,6 +3,8 @@ import style from './mobileArticle.module.css'
 import MobilePresentation from './mobilePresentation/mobilePresentation'
 import MobileAbout from './mobileAbout/mobileAbout'
 import MobileExperience from './mobileExperience/mobileExperience'
+import MobileProjects from './mobileProjects/mobileProjects'
+import MobileContact from './mobileContact/mobileContact'
 
 
 export default function MobileArticle(): JSX.Element {
@@ -12,6 +14,8 @@ export default function MobileArticle(): JSX.Element {
             <MobilePresentation />
             <MobileAbout />
             <MobileExperience />
+            <MobileProjects />
+            <MobileContact />
         </div>
        
     )
