93c3b77084789511ef9fdf4c6681427954b2682a
homero <homerogazze1015@gmail.com>
2022-09-16 15:58:28 -0300

    [UPDATE] Adding some styles to navigation

--- src/mobileComponents/mobileNagivation/mobileNavigation.tsx
+++ src/mobileComponents/mobileNagivation/mobileNavigation.tsx
@@ -5,6 +5,7 @@ import { MdWork } from 'react-icons/md'
 import { AiFillExclamationCircle } from 'react-icons/ai'
 import { ImProfile } from 'react-icons/im'
 import { IoShareSocial } from 'react-icons/io5'
+import { GiPowerButton } from 'react-icons/gi'
 
 import { useRef } from 'react';
 
@@ -42,39 +43,47 @@ export default function MobileNavigation(): JSX.Element {
             </button>
 
             <div ref={options} className={style.options}>
-                <div className={style.navigateOptions}>
-                        
-                         
-                        <Link to="about" spy={true} smooth={true} duration={500} offset={-80}>
-                          <AiFillExclamationCircle className={style.optionIcon}/>
-                          About
-                        </Link>
-                        
-                        <Link to="experience" spy={true} smooth={true} duration={500} offset={-80}>
-                          <FaPhoenixFramework className={style.optionIcon}/>
-                          Experience
-                        </Link>
+                  <div className={style.navigateOptions}>
+                          
+                          
+                          <Link to="about" spy={true} smooth={true} duration={500} offset={-80}>
+                            <AiFillExclamationCircle className={style.optionIcon}/>
+                            About
+                          </Link>
+                          
+                          <Link to="experience" spy={true} smooth={true} duration={500} offset={-80}>
+                            <FaPhoenixFramework className={style.optionIcon}/>
+                            Experience
+                          </Link>
 
-                        <Link to="projects" spy={true} smooth={true} duration={500} offset={-80}>
-                          <MdWork className={style.optionIcon}/>
-                          Projects
-                        </Link>
+                          <Link to="projects" spy={true} smooth={true} duration={500} offset={-80}>
+                            <MdWork className={style.optionIcon}/>
+                            Projects
+                          </Link>
 
-                        <Link to="contact" spy={true} smooth={true} duration={500} offset={-80}>
-                          <FaCommentDots className={style.optionIcon}/>
-                          Contact
-                        </Link>
+                          <Link to="contact" spy={true} smooth={true} duration={500} offset={-80}>
+                            <FaCommentDots className={style.optionIcon}/>
+                            Contact
+                          </Link>
 
-                        <Link to="footer" spy={true} smooth={true} duration={600}>
-                          <IoShareSocial className={style.optionIcon}/>
-                          Social Media
-                        </Link>
-                </div>
+                          <Link to="footer" spy={true} smooth={true} duration={600}>
+                            <IoShareSocial className={style.optionIcon}/>
+                            Social Media
+                          </Link>
 
-                <div className={style.buttonContainer}>
-                    <ImProfile className={style.optionIcon} />
-                    <a href="/resume.pdf">Resume</a>
-                </div>
+                          <div className={style.buttonContainer}>
+                             <ImProfile className={style.optionIcon} />
+                             <a href="/resume.pdf">Resume</a>
+                          </div>
+                  </div>
+
+                  <div className={style.dontPress}>
+                    Don't Press The Button
+                    <GiPowerButton className={style.dontPressIcon} onClick={() => alert("Why you do That...")}/>
+                  </div>
+                    
+                    
+                  
                </div>
                <div onClick={onHiding} ref={overlay} className={style.overlay}></div>
 
